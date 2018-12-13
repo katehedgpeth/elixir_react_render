8cc456a8aa379d146ad9decc56059db21c7e5fd8
Kate Hedgpeth <katehedgpeth@gmail.com>
2018-12-13 15:11:37 -0500

    fix presets

--- priv/server.js
+++ priv/server.js
@@ -5,8 +5,7 @@ const readline = require('readline')
 require('babel-polyfill')
 require('@babel/register')({
   presets: [
-    "@babel/preset-env",
-    "@babel/preset-react"
+    '@babel/env'
   ]
 })
 
